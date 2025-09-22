34c2eef7211005dec483d8aa87a28b5aace92eb5
Rafael Moreira Rosa <156961270+FaelMoreiraRosa@users.noreply.github.com>
2025-09-22 14:11:49 -0300

    Add files via upload

---
+++ Testes/notas.js
@@ -0,0 +1,74 @@
+// Variáveis:
+    //const-> Variável de escopo global, não pode ser reatribuída.
+        /*
+            const nome = "Rafael"
+            console.log(nome)
+        */
+    //let  -> Variável de escopo de bloco, pode ser reatribuída.
+        /*
+            let valor = 30;
+            valor = 45;
+            console.log(valor);
+        */
+    //var  -> Obsoleta
+
+//-------------------------------------------------------------------//
+
+//Tipos de dados:
+    //String -> Texto aspas ou crase("" ou ``) a diferença é que a crase permite pular linhas e chamar variáveis dentro do texto.
+        /*
+            const nome = "Rafael"
+            const sobrenome = `Meu nome é ${nome} da Silva`
+            console.log(sobrenome)
+        */
+    //Number -> Números inteiros ou decimais
+        /*
+            const idade = 30;
+            const altura = 1.75;
+            console.log(idade, altura);
+        */
+    //Boolean -> Verdadeiro ou falso (true ou false)
+        /*
+            const casado = sim = true;
+            const solteiro = nao = false;
+        */
+    //Objetos -> Coleção de dados (chave: valor) usando {}
+        /*
+            const pessoa = {
+                nome: "Rafael",
+                idade: 30,
+                altura: 1.75,
+                casado: true
+            };
+            console.log(pessoa);
+            console.log(pessoa.nome); // Acessando o valor da chave 'nome'
+        */
+    //Null -> Ausência de valor
+        /*
+            const valorNulo = null;
+            console.log(valorNulo); // Exibe 'null'
+        */
+    //Array -> Lista de valores (coleção de dados) usando []
+        /*
+            const frutas = ["maçã", "banana", "laranja"];
+            console.log(frutas); // Exibe a lista de frutas
+            console.log(frutas[0]); // Acessa o primeiro elemento da lista (índice 0)
+        */
+
+//-------------------------------------------------------------------//
+
+// if & else:
+    /*
+        const nota = 5;
+        
+        if(nota >=6) {
+            console.log("Aprovado");
+        } else {
+            console.log("Reprovado");
+        }
+    */
+
+//-------------------------------------------------------------------//
+
+// Funções:
+
